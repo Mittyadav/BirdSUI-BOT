fbe4f740e5e04120bcfa4fe6d18bbe4756da67e8
Mittyadav <146659530+Mittyadav@users.noreply.github.com>
2024-10-25 10:42:11 +0530

    Update banner.js

--- config/banner.js
+++ config/banner.js
@@ -1,4 +1,11 @@
-function printBanner() {                 
+function printBanner() {    
+  console.log("     
+      _____          _____  _  __  _      _____ ______ ______
+     |  __ \   /\   |  __ \| |/ / | |    |_   _|  ____|  ____|
+     | |  | | /  \  | |__) | ' /  | |      | | | |__  | |__
+     | |  | |/ /\ \ |  _  /|  <   | |      | | |  __| |  __|
+     | |__| / ____ \| | \ \| . \  | |____ _| |_| |    | |____
+     |_____/_/    \_\_|  \_\_|\_\ |______|_____|_|    |______|");
   console.log("========== BirdSUI BOT ==========");
   console.log("GitHub: https://github.com/Mittyadav");
   console.log("Telegram: https://t.me/scripthub00");
